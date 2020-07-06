d5cda61003175d4dadc81c421830273b0d624ac4
dtejwani <55606729+dtejwani@users.noreply.github.com>
2020-07-06 16:55:41 +0530

    sixth commit

--- app/src/main/java/com/dhsk/tourguide/RestaurantsFragment.java
+++ app/src/main/java/com/dhsk/tourguide/RestaurantsFragment.java
@@ -29,6 +29,18 @@ public class RestaurantsFragment extends Fragment {
             places.add(new Place(R.string.res3, R.string.res3_add, R.drawable.res3, 4.6));
             places.add(new Place(R.string.res4, R.string.res4_add, R.drawable.res4, 4.1));
             places.add(new Place(R.string.res5, R.string.res5_add, R.drawable.res5, 4.3));}
+
+        else if (i==2){
+            places.add(new Place(R.string.del_res1, R.string.del_res1_add, R.drawable.del_res1, 4.5));
+            places.add(new Place(R.string.del_res2, R.string.del_res2_add, R.drawable.del_res2,4.6));
+            places.add(new Place(R.string.del_res3, R.string.del_res3_add, R.drawable.del_res3,4.4));
+            places.add(new Place(R.string.del_res4, R.string.del_res4_add, R.drawable.del_res4, 4.3));
+            places.add(new Place(R.string.del_res5, R.string.del_res5_add, R.drawable.del_res5, 4.6));
+            places.add(new Place(R.string.del_res6, R.string.del_res6_add, R.drawable.del_res6, 4.4));
+            places.add(new Place(R.string.del_res7, R.string.del_res7_add, R.drawable.del_res7, 4.2));
+            places.add(new Place(R.string.del_res8, R.string.del_res8_add, R.drawable.del_res8, 3.9));
+        }
+
             placeAdapter adapter = new placeAdapter(getActivity(), places, R.color.forstyle);
 
             final ListView listView = (ListView) rootView.findViewById(R.id.list);
