373c707754f0bec1f58a3d2579ea0e3380ce47c4
dtejwani <55606729+dtejwani@users.noreply.github.com>
2020-07-07 16:34:29 +0530

    ninth commit

--- app/src/main/java/com/dhsk/tourguide/MainActivity.java
+++ app/src/main/java/com/dhsk/tourguide/MainActivity.java
@@ -2,8 +2,10 @@ package com.dhsk.tourguide;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView wel_text=findViewById(R.id.wel_tex);
+        String text="<font color=#ff8000>Namaste </font><font color=#008000>India</font>";
+        wel_text.setText(Html.fromHtml(text));
         ImageButton mum_btn=findViewById(R.id.mum_btn);
         mum_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +47,38 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 citynum=3;
+                get_started(citynum);
+            }
+        });
+        ImageButton hyd_btn=findViewById(R.id.hyd_btn);
+        hyd_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                citynum=6;
+                get_started(citynum);
+            }
+        });
+        ImageButton kol_btn=findViewById(R.id.kol_btn);
+        kol_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                citynum=4;
+                get_started(citynum);
+            }
+        });
+        ImageButton chen_btn=findViewById(R.id.chen_btn);
+        chen_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                citynum=7;
+                get_started(citynum);
+            }
+        });
+        ImageButton ahm_btn=findViewById(R.id.ahm_btn);
+        ahm_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                citynum=8;
                 get_started(citynum);
             }
         });
